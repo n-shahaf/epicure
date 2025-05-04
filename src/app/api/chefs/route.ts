2eb768e76120b69f860c46e7dd9da5613d0878ff
Shahaf Noked <shahafn@moveo.co.il>
2025-05-04 17:19:04 +0300

    chefPage styling

--- src/app/api/chefs/route.ts
+++ src/app/api/chefs/route.ts
@@ -2,13 +2,34 @@ import { chefs } from '@/data/chefs.data'
 import { NextResponse } from 'next/server'
 import delay from '@/utils/delay'
 
-export async function GET() {
+enum QueryOptions {
+    ALL = 'all',
+    NEW = 'new',
+    MOST_VIEWED = 'most viewed',
+}
+
+export async function GET(request: Request) {
     await delay(300)
+    const { searchParams } = new URL(request.url)
+    const query = (searchParams.get('q') as QueryOptions) || QueryOptions.ALL
+    let filteredChefs = chefs
 
+    if (query) {
+        switch (query) {
+            case QueryOptions.NEW:
+                filteredChefs = chefs.filter((chef) => chef.isNew)
+                break
+            case QueryOptions.MOST_VIEWED:
+                filteredChefs = chefs.filter((chef) => chef.views > 300)
+                break
+            default:
+                filteredChefs = chefs
+                break
+        }
+    }
     try {
-        return NextResponse.json(chefs, { status: 200 })
+        return NextResponse.json(filteredChefs, { status: 200 })
     } catch (error) {
-
         console.error('Error fetching chefs data', error)
         return NextResponse.json(
             { message: 'Error fetching chefs data' },
